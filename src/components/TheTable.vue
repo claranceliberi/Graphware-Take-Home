981b2d7c49e97ac51a5d2d43854d316dc2b6209a
claranceliberi <liberintwari@gmail.com>
2022-05-25 12:47:52 +0200

    feat: finalizing

--- src/components/TheTable.vue
+++ src/components/TheTable.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 <script lang="ts" setup>
 import type { TableRow } from "@/types";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 interface IProps {
   data: TableRow[];
@@ -28,41 +28,54 @@ const childrens = (data: TableRow) => {
 
   return Object.keys(data.kids);
 };
+
+const active = ref(0);
 </script>
 
 <template>
   <div
     class="relative overflow-x-auto my-2 sm:rounded-lg"
-    :class="props.child ? 'ml-4' : ''"
+    :class="props.child ? 'ml-8' : ''"
   >
     <h2 class="my-2 text-gray-900 text-lg font-semibold">
       {{ props.title.toUpperCase() }}
     </h2>
     <table
-      class="table w-fulltext-sm text-left text-gray-500 dark:text-gray-400"
+      class="table w-full text-sm text-left text-gray-500 dark:text-gray-400"
     >
       <thead
-        class="table-header-group text-xs border-y border-gray-300 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        class="table-header-group font-semibold text-xs border-y border-gray-300 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
       >
         <tr class="table-row">
-          <td class="table-cell px-6 py-3">Song</td>
+          <td class="table-cell px-6 py-3"></td>
           <td class="table-cell px-6 py-3" v-for="head in header" :key="head">
             {{ head }}
           </td>
         </tr>
       </thead>
       <tbody class="table-row-group">
-        <template v-for="data in props.data" :key="data.data[header[0]]">
+        <template
+          v-for="(data, rowNum) in props.data"
+          :key="data.data[header[0]]"
+        >
           <tr
             class="table-row bg-white border-b dark:bg-gray-800 dark:border-gray-700"
           >
             <td class="table-cell px-6 py-3">
-              <button type="reset" v-if="hasChildren(data)">
+              <button
+                type="reset"
+                v-if="hasChildren(data)"
+                @click="
+                  active == rowNum + 1 ? (active = 0) : (active = rowNum + 1)
+                "
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   width="24"
                   height="24"
+                  class="fill-gray-500"
+                  :class="active == rowNum + 1 ? 'rotate-90' : ''"
                 >
                   <path fill="none" d="M0 0h24v24H0z" />
                   <path d="M16 12l-6 6V6z" />
@@ -74,7 +87,10 @@ const childrens = (data: TableRow) => {
             </td>
           </tr>
           <tr>
-            <td v-if="hasChildren(data)" :colspan="header.length + 1">
+            <td
+              v-if="hasChildren(data) && active == rowNum + 1"
+              :colspan="header.length + 1"
+            >
               <TheTable
                 v-for="child in childrens(data)"
                 :data="data.kids[child].records"
